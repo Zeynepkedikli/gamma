56de0140188ef694579eceb10870b48472e88398
Zeynepkedikli <149379387+Zeynepkedikli@users.noreply.github.com>
2026-02-01 05:58:44 +0100

    Add files via upload

---
+++ oop/Computer_Program5/Program.cs
@@ -0,0 +1,106 @@
+﻿//The concepts of OOP in C#
+//Intro 6 encapsulation
+//overloading
+using System;
+using System.Collections.Generic;
+namespace OopInCsharp
+{
+    public class Computer
+    {
+        private string _BIOSname;
+        private string _ipadress;
+        private string _OS;
+        public static int counter = 0;
+        public Computer(string bn, string ip, string os)
+        {
+            _BIOSname = bn;
+            _ipadress = ip;
+            _OS = os;
+            counter += 1;
+        }
+
+        public Computer(string bn, string os)
+        {
+            _BIOSname = bn;
+            _OS = os;
+        }
+
+
+
+        public string BiosName
+        {
+            get { return _BIOSname; }
+            set { _BIOSname = value; }
+        }
+
+        public string IpAdress
+        {
+            get { return _ipadress; }
+            set { _ipadress = value; }
+        }
+
+        public string OS
+        {
+            get { return _OS; }
+            set { _OS = value; }
+        }
+
+
+        public void StartComputer(string num)
+        {
+            IpAdress = num;
+            counter += 1;
+        }
+
+        public static void ShowComps(List<Computer> network)
+        {
+            for (int i = 0; i < network.Count; i++)
+            {
+                Console.WriteLine("{0} {1}", network[i].BiosName, network[i].IpAdress);
+            }
+        }
+
+    }  //the end of tyhe class Computer
+    class Program
+    {
+
+        static string getNum()
+        {
+            Random random = new Random();
+            int num;
+            num = random.Next(1, 255);
+            return num.ToString();
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            List<Computer> net = new List<Computer>();
+
+            int numOfComputers = 4;
+            for (int i = 0; i < numOfComputers; i++)
+
+            {
+                Computer comp = new Computer("comp" + i.ToString(), "10.0.0." + getNum(), "Win10");
+                net.Add(comp);
+            }
+
+
+            Computer.ShowComps(net);
+            Console.WriteLine("We have {0} computers in our network.", Computer.counter);
+
+            //We start a new computer
+            Computer newComp = new Computer("new", "Win10");
+            net.Add(newComp);
+            newComp.StartComputer("10.0.0." + getNum());
+
+            //IT, Math, Physics    
+            Computer.ShowComps(net);
+            Console.WriteLine("We have {0} computers in our network.", Computer.counter);
+
+
+
+        }
+    }
+}
---
+++ oop/Employee_Program1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace CompanyApp
+{
+    public class Employee
+    {
+        protected string Name { get; set; }
+        protected string Position { get; set; }
+        protected double Salary { get; set; }
+        protected int Age { get; set; }
+        public bool Boss { get; protected set; }
+
+        protected static int GenerateAge()
+        {
+            Random random = new Random();
+            return random.Next(18, 65);
+        }
+
+        protected Employee(string name, string pos, double sal, bool boss)
+        {
+            Name = name;
+            Position = pos;
+            Salary = sal;
+            Age = GenerateAge();
+            Boss = boss;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(
+                "Name:{0}, Pos:{1}, Salary:{2}, Age:{3}, Boss:{4}",
+                Name, Position, Salary, Age, Boss
+            );
+        }
+    }
+
+    class Employee1 : Employee
+    {
+        public Employee1() : base("John Smith", "IT Engineer", 15000, false) { }
+    }
+
+    class Employee2 : Employee
+    {
+        public Employee2() : base("Robert Brown", "Military", 80000, false) { }
+    }
+
+    class Employee3 : Employee
+    {
+        public Employee3() : base("Emily White", "Doctor", 50000, false) { }
+    }
+
+    class Employee4 : Employee
+    {
+        public Employee4() : base("Michael Green", "Teacher", 20000, false) { }
+    }
+
+    class Employee5 : Employee
+    {
+        public Employee5() : base("Sarah Black", "Designer", 30000, false) { }
+    }
+
+    class Manager1 : Employee
+    {
+        public Manager1() : base("James Gray", "Designer", 80000, true) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee1();
+            Employee e2 = new Employee2();
+            Employee e3 = new Employee3();
+            Employee e4 = new Employee4();
+            Employee e5 = new Employee5();
+            Employee m1 = new Manager1();
+
+            e1.PrintInfo();
+            e2.PrintInfo();
+            e3.PrintInfo();
+            e4.PrintInfo();
+            e5.PrintInfo();
+
+            Console.WriteLine("\n        Up to the Employee Down to The Manager\n");
+
+            m1.PrintInfo();
+        }
+    }
+}
